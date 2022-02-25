f7b78f753c7e1cfdb3b12cd9302a77f07a071b56
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-25 16:00:22 -0300

    Criando projeto spring-boot com Mongo Db

---
+++ src/test/java/com/valdecirsarat/projetomongodb/ProjetomongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdecirsarat.projetomongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
