14d50a8a6289de103e10e9e4c1c772f9f189afc8
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-28 15:46:01 -0300

    findById e tratamento de exceções

--- src/main/java/com/valdecirsarat/projetomongodb/services/UserService.java
+++ src/main/java/com/valdecirsarat/projetomongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.valdecirsarat.projetomongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.valdecirsarat.projetomongodb.domain.User;
 import com.valdecirsarat.projetomongodb.repository.UserRepository;
+import com.valdecirsarat.projetomongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,11 @@ public class UserService {
 		return repository.findAll();
 		
 	}
-
+	
+	public User findById(String id) {
+		
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+			
+	}
 }
