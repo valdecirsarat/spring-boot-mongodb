d1ecb1cc29109ec3b183351ae80cc2f0a4e2b501
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-28 23:34:32 -0300

    consulta simples com query methods

--- src/main/java/com/valdecirsarat/projetomongodb/services/PostService.java
+++ src/main/java/com/valdecirsarat/projetomongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.valdecirsarat.projetomongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,9 @@ public class PostService {
 			
 	}
 	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 }
 
 
