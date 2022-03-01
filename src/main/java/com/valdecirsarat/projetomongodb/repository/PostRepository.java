095685d029d6f6f4cdf070d9ac458480050b89fd
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-03-01 10:55:13 -0300

    consulta simples @Query

--- src/main/java/com/valdecirsarat/projetomongodb/repository/PostRepository.java
+++ src/main/java/com/valdecirsarat/projetomongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.valdecirsarat.projetomongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.valdecirsarat.projetomongodb.domain.Post;
@@ -11,5 +12,7 @@ import com.valdecirsarat.projetomongodb.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
-
+	
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 }
