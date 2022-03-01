ab2c345b7c17f1fe043a02a3c7b08426bdf90adb
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-28 22:33:44 -0300

    Obetendo posts por ID

---
+++ src/main/java/com/valdecirsarat/projetomongodb/services/PostService.java
@@ -0,0 +1,40 @@
+package com.valdecirsarat.projetomongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.valdecirsarat.projetomongodb.domain.Post;
+import com.valdecirsarat.projetomongodb.repository.PostRepository;
+import com.valdecirsarat.projetomongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	
+	public Post findById(String id) {
+		
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+			
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
